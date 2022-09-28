466428ec42ee78a516a5fe690491bf4387fbbb3b
Nitinjiwnani <jiwnani01@gmail.com>
2022-09-29 03:12:16 +0530

    Get API call with null safety example 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_restapi/example_two.dart';
 
 import 'home_screen.dart';
 
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomeScreen(),
+      home: const ExampleTwo(),
     );
   }
 }
